951c895012dec11ca018bb4bd3e70e0e71ab8bf1
Ruben Quiroz Rangel <rqrsharingan@gmail.com>
2021-07-11 12:53:22 -0500

    first commit

---
+++ src/test/java/com/mx/glass/Glass/GlassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.glass.Glass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
